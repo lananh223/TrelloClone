0744e17230406e4495bb7cd58bfbfe0707a84f7a
Lan <lan.anh.7988@gmail.com>
2023-08-10 15:51:06 -0700

    Add Base Activity

---
+++ app/src/main/java/com/practice/trelloclone/activities/BaseActivity.kt
@@ -0,0 +1,58 @@
+package com.practice.trelloclone.activities
+
+import android.app.Dialog
+import android.os.Bundle
+import android.os.Handler
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
+import com.practice.trelloclone.R
+
+open class BaseActivity : AppCompatActivity() {
+
+    private var doubleBackToExitPressedOnce = false
+    private lateinit var mProgressDialog: Dialog
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    fun showProgressDialog(text: String) {
+        mProgressDialog = Dialog(this)
+        mProgressDialog.setContentView(R.layout.dialog_progress)
+        mProgressDialog.show()
+    }
+
+    fun hideProgressDialog() {
+        mProgressDialog.dismiss()
+    }
+
+    fun getCurrentUserID(): String {
+        return FirebaseAuth.getInstance().currentUser!!.uid
+    }
+    fun doubleBackToExit() {
+        if (doubleBackToExitPressedOnce) {
+            onBackPressedDispatcher
+            return
+        }
+
+        this.doubleBackToExitPressedOnce = true
+        Toast.makeText(
+            this,
+            resources.getString(R.string.please_click_back_again_to_exit),
+            Toast.LENGTH_SHORT
+        ).show()
+
+        Handler().postDelayed({
+            doubleBackToExitPressedOnce = false
+        }, 2000)
+    }
+
+    fun showErrorSnackBar(message: String) {
+        val snackBar =
+            Snackbar.make(findViewById(android.R.id.content),
+            message,
+            Snackbar.LENGTH_LONG).show()
+    }
+}
